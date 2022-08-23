d1719da0c575751906e6513287e7a724b6448d30
codesari <ahmtsr81@gmail.com>
2022-08-23 18:03:18 +0300

    ex1 is finished

--- exercise-1.js
+++ exercise-1.js
@@ -1,2 +1,26 @@
-//!Example sum of positive numbers, when user enters a negative number
-console.log("********* EXERCİSE """"""""""""")
+//!---------- 1 ---------
+//! Example sum of positive numbers, when user enters a negative number
+console.log("********* EXERCİSE ***********");
+
+//? Negatif sayi girilene kadar girilen pozitif sayıları topla
+//! do-while methods
+// let num = 0;
+// let sum = 0;
+
+// do {
+//   sum += num;
+//   num = Number(prompt("Enter a number,please"));
+// } while (num >= 0);
+
+// console.log(sum);
+
+//! only while methods
+
+let num = Number(prompt("Enter a number,please"));
+let sum = 0;
+
+while (num >= 0) {
+  sum += num;
+  num = Number(prompt("Enter a number,please"));
+}
+console.log(sum);
