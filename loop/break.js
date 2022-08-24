5663d1089b8fc1a1a7cfa82a9332b4b3be409ab4
codesari <ahmtsr81@gmail.com>
2022-08-24 20:32:11 +0300

    first commit

--- loop/break.js
+++ loop/break.js
@@ -1,8 +1,20 @@
 //! Example print numbers from 0 to 7 using break
+//!break
 
-let num = 0;
-while (true) {
-  alert(num);
-  num++;
-  if (num == 7) break;
+// let num = 0;
+// while (true) {
+//   alert(num);
+//   num++;
+//   if (num == 7) break;
+// }
+
+//! What is the output of the following code?
+//!continue
+var sum = 0;
+for (var i = 3; i < 7; i++) {
+  sum += i;
+  if (i === 3 || sum > 10) {
+    continue;
+  }
+  console.log(i);
 }
---
+++ strings/string.js
@@ -0,0 +1,17 @@
+// ?======================================================
+// ?                     STRING METOTLARI
+// ! KAYNAK: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// ?======================================================
+
+console.log("*** STRING METHODS ***");
+//ilkel yöntemle tanımlanan string daha efektif bellek açısından
+const str1 = "Course";
+const str2 = "Hello";
+const str3 = "World";
+
+console.log(str1, typeof str1);
+
+//!Non-primitive string tanımlama
+
+const str4 = new String("Hello FS");
+console.log(typeof str4);
