df784e9a75e09f784bef6d88570a3be77f7b1ca5
codesari <ahmtsr81@gmail.com>
2022-08-27 11:28:50 +0300

    concatination

--- strings/string.js
+++ strings/string.js
@@ -1,4 +1,15 @@
+//! String Literals (klasik yöntem) ile tanımlama.(Primitive)
 const str1 = "Ahmet";
-const str2 = "Sarı";
-console.log(str1 + " " + str2);
-console.log(typeof str1);
+const str2 = "Sari";
+// console.log(str1 + " " + str2);
+console.log(str1, typeof str1);
+
+//! Non-primitive string tanımlama.(Object)
+
+const str3 = new String("Object olarak tanimlama");
+console.log(typeof str3);
+
+//! String birleştirme (Concatination)
+const sayi = 3;
+const str4 = str1 + str2 + str3 + sayi;
+console.log(str4);
