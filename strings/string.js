20e27507bd0c7e5b3e3e40c16b1664e740b949f1
codesari <ahmtsr81@gmail.com>
2022-08-27 11:16:06 +0300

    some changes is done

--- strings/string.js
+++ strings/string.js
@@ -1,17 +1,4 @@
-// ?======================================================
-// ?                     STRING METOTLARI
-// ! KAYNAK: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
-// ?======================================================
-
-console.log("*** STRING METHODS ***");
-//ilkel yöntemle tanımlanan string daha efektif bellek açısından
-const str1 = "Course";
-const str2 = "Hello";
-const str3 = "World";
-
-console.log(str1, typeof str1);
-
-//!Non-primitive string tanımlama
-
-const str4 = new String("Hello FS");
-console.log(typeof str4);
+const str1 = "Ahmet";
+const str2 = "Sarı";
+console.log(str1 + " " + str2);
+console.log(typeof str1);
