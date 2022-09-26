ac8fef2e3205ce8a38c401fa76657b8964288a8e
codesari <ahmtsr81@gmail.com>
2022-09-26 21:32:31 +0300

    news apı is finished

--- js19_async2/async-await/async-await-2.js
+++ js19_async2/async-await/async-await-2.js
@@ -1,14 +1,20 @@
+let isError = false;
+
 getTechNews = async () => {
   const apiKey = "6cefe118ac8c443ba227b92f540b086d";
   //   const country
   //   const category
   const url = `https://newsapi.org/v2/top-headlines?country=tr&category=technology&apiKey=${apiKey}`;
+  //* bir hata olustugunda kullanıcı bu hatayı göremiyor,hata console da gözüküyor.biz bu hatayı kullanıcıya bilgi vermek icin DOM'a basmamiz gerekiyor.bunun icin flag mantığıyla bir degisken olusturup kontrol edelim.
 
   try {
     const res = await fetch(url);
     //   console.log(res);
     if (!res.ok) {
-      throw new Error(`Something went wrong.Error Code --> ${res.status}`);
+      isError = true;
+      //* throw new Error,hatayı consola yazdırır.biz kullanıcıya bilgi vermek için DOM'a basmak istiyoruz
+      //   throw new Error(`Something went wrong.Error Code --> ${res.status}`);
+      //* burada bir hata fırlattıgimizda program direk catch'e gidecektir.bu yüzden asagidaki dom'a basma fonksiyonu calismayacak ve ekrana bilgi veremeyecegiz bu yüzden hata fırlatma kodunu siliyoruz onun yerine kullanıcıya viz bilgi veriyoruz
     }
     const data = await res.json();
     //! eger await yazmazsak verinin gelmesini beklemeden asagi gecebilir ve veriler null gözükebilir
@@ -22,6 +28,12 @@ getTechNews = async () => {
 printToDOM = (news) => {
   console.log(news);
   const newsLists = document.querySelector("#news-lists");
+  if (isError) {
+    newsLists.innerHTML += `<h2> Error occured.</h2>
+    <img src="./img/404.png" alt="">`;
+    return;
+    //* bu sekilde return dersek,artik asagi satira bakma anlamına gelir
+  }
   news.forEach((item) => {
     const { title, description, urlToImage, url } = item; //! dest..
     newsLists.innerHTML += `
