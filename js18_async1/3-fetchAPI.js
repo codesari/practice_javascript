35ce01ac00415f04d96ed43ba13e1aba3e552d45
codesari <ahmtsr81@gmail.com>
2022-09-26 12:11:30 +0300

    extract to json data

--- js18_async1/3-fetchAPI.js
+++ js18_async1/3-fetchAPI.js
@@ -5,20 +5,25 @@
 //   .then((res) => res.json())
 //   .then((data) => console.log(data))
 //   .catch((err) => console.log(err));
-
-// ! fetch,optimist bir yapı.cevap geldigi müddetce kendini basarılı sayıyor,hatalar bu yüzden catch e girmiyor.hatayı catch'de göstermek için gelen responsa üzerinden status durumunu kontrol ettiriyoruz.ok degiskeni true ve false degeri alır.true ise baglanti basarili olmustur,false ise basarisiz
-fetch("https://api.github.com/user")
+//
+// ! fetch,optimist bir yapı.cevap geldigi müddetce kendini basarılı sayıyor,kendine yanlızca response gelmezse basarisiz sayıyor.hatalar bu yüzden catch e girmiyor 401 vs...hatayı catch'de göstermek için gelen responsa üzerinden status durumunu kontrol ettiriyoruz.ok degiskeni true ve false degeri alır.true ise baglanti basarili olmustur,false ise basarisiz
+// * url sonu : users -->true
+fetch("https://api.github.com/users")
   .then((res) => {
     console.log(res);
+    // * Error Handling
     if (!res.ok) {
       throw Error("Something went wrong");
     } else {
       //? Hata yoksa veriyi json lastir.
       return res.json();
+      //? res.json() --> data
     }
   })
   .then((data) => console.log(data))
   .catch((err) => console.log(err));
+
+//! fetch,asenkron bir yapı,res.json() ile okudugumuz verileri fetch dısında göstermek istediğimiz zaman ve bu yapı senkron olacağından verileri göremiyoruz.çünkü önce senkron islem calisiyor daha sonra fetch den veriler geliyor dolayısıyla görmek istediğimiz veriyi göremiyoruz
 
 // fetch("https://api.github.com/users")
 //   //? aksi belirtilmedikçe burası GET metoduduriGET harici bir kullanım icin method:POST diye belirtmek gerekir
