b757feabcde29c2f2258946dce7c4447f2a9db5c
codesari <ahmtsr81@gmail.com>
2022-10-01 09:26:01 +0300

    oop studying

--- js21_oop1/1-objectLiteral.js
+++ js21_oop1/1-objectLiteral.js
@@ -5,28 +5,31 @@
 //* Object Literals
 console.log("**** Object Literals ****");
 
-const book1 = {
-  title: "ISID'la Mücadelem",
-  author: "Nadia Murad",
-  year: 2008,
-  // !arrow fonk. burada dogru calismiyor.this calismiyor
-  getSummary: function () {
-    return ` ${this.title} was written by ${this.author} in ${this.year}`;
-  },
-};
-console.log(book1);
-console.log(book1.hasOwnProperty("author")); //? true
-// ! Object nesnesinden miras yoluyla geldi..
-console.log(new Date());
+// console.log(new Date());
 
-const book2 = {
-  title: "Outliers",
-  author: "Adam Fewer",
-  year: 2020,
-  // !arrow fonk. burada dogru calismiyor.this calismiyor
+// const book2 = {
+//   title: "Outliers",
+//   author: "Adam Fewer",
+//   year: 2020,
+//   // !arrow fonk. burada dogru calismiyor.this calismiyor
+//   getSummary: function () {
+//     return ` ${this.title} was written by ${this.author} in ${this.year}`;
+//   },
+// };
+// console.log(book2);
+// console.log(book2.getSummary());
+
+const person = {
+  name: "Ahmet",
+  age: "30",
+  location: "Akçakoca",
   getSummary: function () {
-    return ` ${this.title} was written by ${this.author} in ${this.year}`;
+    return `${this.name},${this.age} years old and he lives in ${this.location}`;
+    // ! burada keys lere ulaşmak için this kullanmayı unutmuyoruz
   },
 };
-console.log(book2);
-console.log(book2.getSummary());
+
+console.log(person);
+console.log(person.getSummary());
+//? gelen person objesi içinde prototyp özellikleri bulunuyor.bir tanesini deneyelim.
+console.log(person.hasOwnProperty("age"));
