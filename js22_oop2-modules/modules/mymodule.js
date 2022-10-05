774cfe6689488c35f7c333f9b0fea83966c7ad50
codesari <ahmtsr81@gmail.com>
2022-10-05 13:05:18 +0300

    export module

--- js22_oop2-modules/modules/mymodule.js
+++ js22_oop2-modules/modules/mymodule.js
@@ -1,34 +1,55 @@
+// function add(n1, n2) {
+//   return n1 + n2;
+// }
+
+// const WORKING_HOURS_WEEK = 40; // 45 , 30
+
+// let counter = 0;
+// let localCounter = 0;
+
+// function inc(amount = 1) {
+//   counter += amount;
+//   return counter;
+// }
+
+// function dec(amount = 1) {
+//   counter -= amount;
+//   return counter;
+// }
+
+// const moduleName = "Module 1";
+// // export default moduleName;
+
+// export default (num) => num * num;
+
+// export {
+//   WORKING_HOURS_WEEK,
+//   WORKING_HOURS_WEEK as WHW,
+//   add as sum,
+//   inc,
+//   dec,
+//   counter,
+// };
+
+// console.log("module 1 is loaded");
+
 function add(n1, n2) {
   return n1 + n2;
 }
-
-const WORKING_HOURS_WEEK = 40; // 45 , 30
-
+const WORKING_HOURS_WEEK = 40;
 let counter = 0;
-let localCounter = 0;
 
 function inc(amount = 1) {
   counter += amount;
   return counter;
 }
-
 function dec(amount = 1) {
   counter -= amount;
   return counter;
 }
-
 const moduleName = "Module 1";
 // export default moduleName;
-
 export default (num) => num * num;
 
-export {
-  WORKING_HOURS_WEEK,
-  WORKING_HOURS_WEEK as WHW,
-  add as sum,
-  inc,
-  dec,
-  counter,
-};
-
-console.log("module 1 is loaded");
+//* export etmek istediklerimi yani paylaşmak istediklerimi belirtiyorum
+export { WORKING_HOURS_WEEK };
